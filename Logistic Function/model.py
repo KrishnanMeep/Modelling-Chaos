47e3cdad88bfb07060f1d9d109ea8f04d28907ff
krishnanmeep <96meep96@gmail.com>
2019-09-15 21:04:33 +0530

    Logistic Function

--- Logistic Function/model.py
+++ Logistic Function/model.py
@@ -35,7 +35,7 @@ def load_data(filename, seq_length):
 
 filename = 'logistic3.9.csv'
 batch_size = 128
-seq_length = 7
+seq_length = 1
 x_train, y_train, c_train, x_test, y_test, c_test = load_data(filename, seq_length)
 print(len(x_train), "training sequences")
 print(len(x_test), "test sequences")
@@ -43,28 +43,30 @@ print(batch_size, "batch size")
 
 
 #BuildGraph
+tf.reset_default_graph()
+
 x = tf.placeholder(tf.float32, [None, None, 1])
 y = tf.placeholder(tf.float32, [None, 1])
 c = tf.placeholder(tf.float32, [None, 1])
 
-rnn_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(128), tf.nn.rnn_cell.BasicLSTMCell(256), tf.nn.rnn_cell.BasicLSTMCell(512)])
+rnn_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(16), tf.nn.rnn_cell.BasicLSTMCell(32), 
+										tf.nn.rnn_cell.BasicLSTMCell(64)])
 outputs, states = tf.nn.dynamic_rnn(cell = rnn_cell, inputs = x, dtype = tf.float32)
 
-Cat1 = tf.concat([outputs[:,-1], c], axis = 1)
-
-#D1 = tf.layers.dense(outputs[:, -1], 1)
+Cat1 = tf.concat([outputs[:, -1], c], axis = 1)
 D1 = tf.layers.dense(Cat1, 1)
+A1 = tf.nn.sigmoid(D1)
 
 #Loss
-L = tf.reduce_mean(tf.square(D1 - y))
-trainer = tf.train.RMSPropOptimizer(learning_rate = 0.0003).minimize(L)
+L = tf.reduce_mean(tf.square(A1 - y))
+trainer = tf.train.AdamOptimizer(learning_rate = 0.0003).minimize(L)
 
 print(x_train.shape, y_train.shape)
 
 init = tf.global_variables_initializer()
 iterations = (len(x_train) + len(x_test))//batch_size
-epochs = 300
-reset = True
+epochs = 0
+reset = False
 saver = tf.train.Saver()
 model_path = "./models/logistic3.9.ckpt"
 
@@ -91,30 +93,62 @@ with tf.Session() as sess:
 
 	print("Done :", timedelta(seconds = time.time() - start))
 
-	#starter = np.array([x_test[0, 0]])
-	starter = np.array(x_test[0])
+	TrainMSE = 0
 
-	print("Starter", starter)
+	steps = len(x_train)//500-30
 
-	predicted = list(starter.ravel())
-	beg = starter[0]
+	for i in range(steps):
+		print("Training points", i, "/", steps)
 
-	for i in range(seq_length*9):
-		pred = sess.run(D1, feed_dict = {x : [starter], c : [beg]})[0]
-		starter = np.vstack((starter[1:], pred))
-		predicted.append(pred[0])
+		starter = np.array([x_train[i, 0]])
+
+		predicted = list(starter.ravel())
+		beg = starter
+
+		for _ in range(seq_length*30-1):
+			pred = sess.run(A1, feed_dict = {x : [starter], c : beg})[0]
+			starter = np.vstack((starter[1:], pred))
+			predicted.append(pred[0])
 
 
-	predicted = np.array(predicted).ravel()
-	actuals = x_test[0:10].ravel()
+		predicted = np.array(predicted).ravel()
+		actuals = x_train[i:i+30].ravel()
 
-	print("Actuals: ", actuals)
-	print("Predicted: ", predicted)
+		TrainMSE += np.square(actuals-predicted).mean()
+		
+	print("Train MSE Loss :", round(TrainMSE/steps, 4))  
 
-	MSE = np.square(actuals-predicted).mean()
-	print("Test MSE Loss :", round(MSE, 4))
+	TestMSE = 0  
+	steps = len(x_test)//500-30
 
-	plt.plot(seq_length, actuals[seq_length], 'go')
+	for i in range(steps):
+		print("Testing points", i, "/", steps)
+
+		starter = np.array([x_test[i, 0]])
+
+		predicted = list(starter.ravel())
+		beg = starter
+
+		for _ in range(seq_length*30-1):
+			pred = sess.run(A1, feed_dict = {x : [starter], c : beg})[0]
+			starter = np.vstack((starter[1:], pred))
+			predicted.append(pred[0])
+
+
+		predicted = np.array(predicted).ravel()
+		actuals = x_test[i:i+30].ravel()
+
+		TestMSE += np.square(actuals-predicted).mean()
+		
+	print("Test MSE Loss :", round(TestMSE/steps, 4))
+
+	plt.rcParams["figure.figsize"] = (25,10)
+
+
+	plt.plot(seq_length, actuals[seq_length], 'gx', alpha = 0.75)
 	plt.plot(actuals, 'r', alpha = 0.8, label = 'Acutal')
 	plt.plot(predicted.ravel(), 'b:', alpha = 0.7, label = 'Predicted')
+	plt.title("Initial point " + str(beg))
+	plt.legend()
+
 	plt.show()
